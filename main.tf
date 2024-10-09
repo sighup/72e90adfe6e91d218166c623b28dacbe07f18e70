a8ec90c1f69eb7821df3a356050250cb978804bb
Matt Saliano <matt.saliano@liatrio.com>
2024-10-09 12:08:41 -0500

    init

---
+++ main.tf
@@ -0,0 +1,33 @@
+# Define the required provider
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Input variable to define the number of random strings
+variable "number_of_strings" {
+  description = "Number of random strings to generate"
+  type        = number
+  default     = 3
+}
+
+# Generate random strings using a for_each loop
+resource "random_string" "strings" {
+  count = var.number_of_strings
+
+  length  = 16
+  special = false
+  upper   = true
+  lower   = true
+  number  = true
+}
+
+# Output the generated random strings
+output "random_strings" {
+  value = random_string.strings[*].result
+}
+
